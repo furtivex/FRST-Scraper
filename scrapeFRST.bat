0489580d018cb9941ebe785a634e46b5beba2f55
d1savow3d <35790653+furtivex@users.noreply.github.com>
2025-04-20 10:45:04 -0500

    some changes

--- scrapeFRST.bat
+++ scrapeFRST.bat
@@ -2,33 +2,28 @@
 :: Created by Furtivex
 @ECHO OFF
 @SETLOCAL
-@PROMPT #
 SET "downloads=C:\Users\d1savow3d\Downloads"
 
-DEL /F/Q "%TEMP%\Clipboard*" >NUL 2>&1
+DEL /F/A/Q "%TEMP%\Clipboard*" >NUL 2>&1
 
-DIR /B/S/A:-D "%downloads%\FRST*.txt" 2>NUL>"%TEMP%\clipboard00"
-DIR /B/S/A:-D "%downloads%\Addition*.txt" 2>NUL>>"%TEMP%\clipboard00"
-DIR /B/S/A:-D "%downloads%\Fixlog*.txt" 2>NUL>>"%TEMP%\clipboard00"
-DIR /B/S/A:-D "%downloads%\Fixlist*.txt" 2>NUL>>"%TEMP%\clipboard00"
+DIR /B/S/A:-D "%downloads%\FRST*.txt" 2>NUL>"%TEMP%\Clipboard00"
+DIR /B/S/A:-D "%downloads%\Addition*.txt" 2>NUL>>"%TEMP%\Clipboard00"
 
-
-FOR /F "TOKENS=*" %%G IN ( %TEMP%\clipboard00 ) DO @(
+FOR /F "TOKENS=*" %%G IN ( %TEMP%\Clipboard00 ) DO @(
   TYPE "%%G">>"%TEMP%\Clipboard01"
   )
 
-GREP -Eis "detected!|\[X\]|== (ATTENTION|ВНИМАНИЕ|Cyrillic)|AlternateDataStreams|\\StartupApproved\\|\\MountPoints|\(?No File\)?|\(?Нет файла\)?|no ImagePath|(ATTENTION|ВНИМАНИЕ):|zero byte File|not found|no filepath" <"%TEMP%\Clipboard01" >"%TEMP%\Clipboard02"
+
+GREP -Eis "ATTENTION:?|\(?No (ImagePath|filepath|File)\)?|zero byte File|not found|detected!|AlternateDataStreams|\\StartupApproved\\|\\MountPoints|\[X\]" <"%TEMP%\Clipboard01" >"%TEMP%\Clipboard02"
 GREP -Pis "(Page|URL) =\s+?$|grubx64\.efi|\{[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}\}] -\> $|\(Allow\) LPort=\d{2,5}|DisallowedCertificates" <"%TEMP%\Clipboard01" >>"%TEMP%\Clipboard02"
-GREP -Eis "\[(File not signed|Файл не подписан)\]|000000000 __SHD|File is in use" <"%TEMP%\Clipboard01" >"%TEMP%\Clipboard05"
-SORT_ -f -u <"%TEMP%\Clipboard02" >"%TEMP%\Clipboard03"
-SORT_ -f -u <"%TEMP%\Clipboard05" >"%TEMP%\Clipboard06"
+GREP -Eis "\[File not signed\]|000000000 __SHD|File is in use" <"%TEMP%\Clipboard01" >"%TEMP%\Clipboard03"
 
 ECHO.Start::>"%TEMP%\ClipboardFinal.txt"
 ECHO.SystemRestore: On>>"%TEMP%\ClipboardFinal.txt"
 ECHO.CreateRestorePoint:>>"%TEMP%\ClipboardFinal.txt"
 ECHO.CloseProcesses:>>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
-TYPE "%TEMP%\Clipboard03">>"%TEMP%\ClipboardFinal.txt"
+TYPE "%TEMP%\Clipboard02">>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
 ECHO.Removeproxy:>>"%TEMP%\ClipboardFinal.txt"
@@ -118,7 +113,7 @@ ECHO.>>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
 ECHO.>>"%TEMP%\ClipboardFinal.txt"
-TYPE "%TEMP%\Clipboard06">>"%TEMP%\ClipboardFinal.txt"
+TYPE "%TEMP%\Clipboard03">>"%TEMP%\ClipboardFinal.txt"
 
 "C:\Program Files\Notepad2\Notepad2.exe" "%TEMP%\ClipboardFinal.txt"
-DEL /F/Q "%TEMP%\Clipboard0?" >NUL 2>&1
+DEL /F/A/Q "%TEMP%\Clipboard0?" >NUL 2>&1
